70d1aaf6f9b2d8567410c1814ee3027fa22c931e
Dennis Bartuschat <dennis.bartu@gmail.com>
2014-07-21 22:32:56 +0200

    anapssungen layout

---
+++ data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: debartu
+ * Date: 21.07.14
+ * Time: 17:54
+ */
+
+var_dump($_POST);
+if(isset($_POST['reqBubble'])){
+	return "ERFOLG";
+}
--- js/app/scroller.js
+++ js/app/scroller.js
@@ -4,5 +4,30 @@ App.scroller = {
 			useCSSTranslation: false,
 			constrainTo: 'parent'
 		});
-    }
+		this.overlay();
+    },
+
+	overlay : function(){
+		$('.bubble').on('click', function(){
+			var reqBubble = this.id || false;
+
+			if(this.id)
+				App.scroller.getContent(reqBubble);
+		});
+	},
+
+	showOverlay : function(data){
+			console.log(data);
+	},
+
+	getContent : function(reqBubble){
+		$.ajax({
+			url 	:	'data.php',
+			params	:	reqBubble,
+			type	:	'POST',
+			success	:	function(data){
+				App.scroller.showOverlay(data);
+			}
+		});
+	}
 }
