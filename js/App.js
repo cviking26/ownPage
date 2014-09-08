000f97e9dafbfd8b1056e8425024e05b0f2560d6
Dennis Bartuschat <dennis.bartu@gmail.com>
2014-09-08 13:14:33 +0200

    anapssungen layout

--- js/App.js
+++ js/App.js
@@ -1,5 +1,13 @@
 App = {
     init : function (){
-        App.scroller.init();
-    }
+        App.start();
+    },
+	start : function (){
+		/*$( ".self-picture-bottom" ).switchClass( "self-picture-bottom", "self-picture-top", 2000, "easeInCirc", function(){
+			move(".header-right").set('opacity', '1').duration('2s').end();
+			$('main').fadeIn(2000);
+			$('footer').fadeIn(2000);
+		});*/
+		//move('#header-left-span-wrap').set('float', 'none').duration('5s').end();
+	}
 }
